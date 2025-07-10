76cb623a08da3a6510ebe730aa81cf0dea308127
Marcos Vinicius Felix <mvfelix2011@hotmail.com>
2025-07-10 12:10:53 -0300

    Correção no  bloco do <nav> .

--- script.js
+++ script.js
@@ -67,4 +67,12 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
   });
 
   observe.observe(banne);
+
+  const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
   
